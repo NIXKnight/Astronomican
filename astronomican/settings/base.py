63d554d526f5baf0b5677ed777dabdcf021a402b
Saad Ali <saad@nixknight.net>
2022-05-03 10:18:53 +0500

    Added Dynaconf to read configuration files

---
+++ astronomican/settings/base.py
@@ -0,0 +1,46 @@
+from pathlib import Path
+
+BASE_DIR = Path(__file__).resolve().parent.parent
+
+INSTALLED_APPS = [
+    'django.contrib.sessions',
+    'django.contrib.messages',
+    'django.contrib.staticfiles',
+]
+
+MIDDLEWARE = [
+    'django.middleware.security.SecurityMiddleware',
+    'django.contrib.sessions.middleware.SessionMiddleware',
+    'django.middleware.common.CommonMiddleware',
+    'django.middleware.csrf.CsrfViewMiddleware',
+    'django.contrib.messages.middleware.MessageMiddleware',
+    'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'basicauth.middleware.BasicAuthMiddleware',
+]
+
+ROOT_URLCONF = 'astronomican.urls'
+
+TEMPLATES = [
+    {
+        'BACKEND': 'django.template.backends.django.DjangoTemplates',
+        'DIRS': [],
+        'APP_DIRS': True,
+        'OPTIONS': {
+            'context_processors': [
+                'django.template.context_processors.debug',
+                'django.template.context_processors.request',
+                'django.contrib.auth.context_processors.auth',
+                'django.contrib.messages.context_processors.messages',
+            ],
+        },
+    },
+]
+
+WSGI_APPLICATION = 'astronomican.wsgi.application'
+
+DATABASES = {}
+
+USE_I18N = True
+USE_TZ = True
+
+STATIC_URL = 'static/'
